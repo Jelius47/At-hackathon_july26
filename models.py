6150bae801cb4b8d169eab91ae4f5de1a984a67c
Jelius <jeliusheneriko47@gmail.com>
2025-07-26 13:31:50 +0300

    jelius add: database modify: main models

--- models.py
+++ models.py
@@ -2,15 +2,17 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Float
 from sqlalchemy.orm import relationship, declarative_base
 
 Base = declarative_base()
-
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     phone_number = Column(String, nullable=False, unique=True)
+    email = Column(String, nullable=False, unique=True)
+    password_hash = Column(String, nullable=False)
 
     plots = relationship('Plot', back_populates='owner')
+
 
 
 class Drone(Base):
